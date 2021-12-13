2e76afb85bbf5e667dfe7db59b2be6b326f354e1
KGin2000 <72448004+KGin2000@users.noreply.github.com>
2021-12-14 02:56:19 +0700

    Add Character

---
+++ PlayerZero/Assets/Scripts/CharControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharControl : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement);
+    }
+}
