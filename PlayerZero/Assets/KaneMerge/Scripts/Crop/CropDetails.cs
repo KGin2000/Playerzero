7864ad12fd89c88e638a719e7d5a45f8dcee25cb
PingPansa <61050250@kmitl.ac.th>
2022-05-09 20:12:25 +0700

    MegerK2

--- PlayerZero/Assets/KaneMerge/Scripts/Crop/CropDetails.cs
+++ PlayerZero/Assets/KaneMerge/Scripts/Crop/CropDetails.cs
@@ -36,10 +36,12 @@ public class CropDetails
     {
         if (RequairedHarvestActionsForTool(toolItemCode) == -1)
         {
+            Debug.Log("IF");
             return false;
         }
         else
         {
+            Debug.Log("ELSE");
             return true;
         }
     }
